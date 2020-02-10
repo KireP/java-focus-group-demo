0f2f30ea34120cfba8f20a6aeda258ceb4e01396
KireP <kire_m.j@hotmail.com>
2020-02-10 17:28:23 +0100

    Eureka, config, and Zuul server added

---
+++ configuration-server/src/main/docker/run.sh
@@ -0,0 +1,10 @@
+echo "********************************************************"
+echo "Waiting for the eureka server to start  on port $EUREKASERVER_PORT"
+echo "********************************************************"
+while ! `nc -z eurekaserver $EUREKASERVER_PORT`; do sleep 3; done
+echo ">>>>>>>>>>>> Eureka Server has started"
+
+echo "********************************************************"
+echo "Starting Configuration Server"
+echo "********************************************************"
+java -Djava.security.egd=file:/dev/./urandom -Deureka.client.serviceUrl.defaultZone=$EUREKASERVER_URI -jar /usr/local/configurationserver/@project.build.finalName@.jar
