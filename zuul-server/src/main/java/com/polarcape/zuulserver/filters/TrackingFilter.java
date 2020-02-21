837cb06209a8a9a34f155fd6743987546c242a95
KireP <kire_m.j@hotmail.com>
2020-02-21 16:11:55 +0100

    Zuul tracking filter added

---
+++ zuul-server/src/main/java/com/polarcape/zuulserver/filters/TrackingFilter.java
@@ -0,0 +1,41 @@
+package com.polarcape.zuulserver.filters;
+
+import com.netflix.zuul.ZuulFilter;
+import com.netflix.zuul.context.RequestContext;
+import com.netflix.zuul.exception.ZuulException;
+import com.polarcape.zuulserver.util.FilterUtilConstants;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.stereotype.Component;
+
+@Component
+public class TrackingFilter extends ZuulFilter {
+
+    private static final Logger logger = LoggerFactory.getLogger(TrackingFilter.class);
+
+    private static final int FILTER_ORDER = 1;
+    private static final boolean SHOULD_FILTER = true;
+
+    @Override
+    public String filterType() {
+        return FilterUtilConstants.PRE_FILTER_TYPE;
+    }
+
+    @Override
+    public int filterOrder() {
+        return FILTER_ORDER;
+    }
+
+    @Override
+    public boolean shouldFilter() {
+        return SHOULD_FILTER;
+    }
+
+    @Override
+    public Object run() throws ZuulException {
+        logger.debug("Correlation ID: {}", RequestContext.getCurrentContext().getRequest().getHeader(FilterUtilConstants.CORRELATION_ID));
+        logger.debug("Authentication token: {}", RequestContext.getCurrentContext().getRequest().getHeader(FilterUtilConstants.AUTHENTICATION_TOKEN));
+        logger.debug("===============================================================================");
+        return null;
+    }
+}
---
+++ zuul-server/src/main/java/com/polarcape/zuulserver/util/FilterUtilConstants.java
@@ -0,0 +1,10 @@
+package com.polarcape.zuulserver.util;
+
+public class FilterUtilConstants {
+
+    public static final String CORRELATION_ID = "pc-correlation-id";
+    public static final String AUTHENTICATION_TOKEN = "pc-authentication-token";
+    public static final String PRE_FILTER_TYPE = "pre";
+    public static final String POST_FILTER_TYPE = "post";
+    public static final String ROUTE_FILTER_TYPE = "route";
+}
